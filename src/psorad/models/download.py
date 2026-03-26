fa0f4309d54589b57ea89ca5d8ae685c91c7d7c4
CaoTurkey <Cao-Turkey@outlook.com>
2026-03-26 09:59:29 +0800

    fix: remove local_dir_use_symlinks parameter from snapshot_download call

--- src/psorad/models/download.py
+++ src/psorad/models/download.py
@@ -28,7 +28,7 @@ def download_siglip(
 
     target_dir = Path(save_dir)
     target_dir.mkdir(parents=True, exist_ok=True)
-    snapshot_path = snapshot_download(repo_id=repo_id, local_dir=str(target_dir), local_dir_use_symlinks=False)  # type: ignore[call-overload]
+    snapshot_path = snapshot_download(repo_id=repo_id, local_dir=str(target_dir))
     return Path(snapshot_path)
 
 
